81edc75b6b01b4f8f2bc328820293fa70a94f69e
Richard (Zheng) Sun <zhensn@gmail.com>
2015-08-29 17:08:19 -0700

    first commit

---
+++ Classifier.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaiveBayesClassifier
+{
+    class Classifier
+    {
+        TrainingSet trainingSet = new TrainingSet();
+
+        // runs Naive Bayes classification and returns outcome with most probability
+        public string classify(Dictionary<string, Tuple<int, int>> myEvidence)
+        {
+            Double maxProbability = 0;
+            string maxOutcome = "";
+
+            // p(O | x_1, x_2, ... x_n) = (p(x_1 | O) * p(x_2 | O) * ... p(x_n | O) * p(O)) / (p(x_1) * p(x_2) * ... p(x_n))
+            foreach (KeyValuePair<string, Dictionary<string, Tuple<int, int>>> outcomeEntry in trainingSet.Outcomes)
+            {
+                Double entryProbability = 1;
+
+                foreach (KeyValuePair<string, Tuple<int, int>> evidenceEntry in outcomeEntry.Value)
+                {
+                    if (myEvidence.ContainsKey(evidenceEntry.Key))
+                    {
+                        entryProbability *= (evidenceEntry.Value.Item1 / (double) evidenceEntry.Value.Item2) /
+                            (trainingSet.TotalEvidences[evidenceEntry.Key].Item1 / (double) trainingSet.TotalEvidences[evidenceEntry.Key].Item2);
+                    }
+                }
+
+                entryProbability *= (trainingSet.OutcomeTotals[outcomeEntry.Key] / (double) trainingSet.TotalOutcomeEntries);
+
+                if (entryProbability > maxProbability)
+                {
+                    maxProbability = entryProbability;
+                    maxOutcome = outcomeEntry.Key;
+                }
+            }
+
+            return maxOutcome;
+        }
+    }
+}
---
+++ TrainingSet.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaiveBayesClassifier
+{
+    // stores TrainingSet data for classifier
+    class TrainingSet
+    {
+        // stores all outcomes and their corresponding evidence true and totals 
+        // <Outcome : <Evidence : (True, Total)>>
+        private Dictionary<string, Dictionary<string, Tuple<int, int>>> outcomeSet = 
+            new Dictionary<string, Dictionary<string, Tuple<int, int>>>();
+
+        // stores all evidences and their true and total amounts summed from all outcomes
+        // <Evidence, (True, Total)>
+        private Dictionary<string, Tuple<int, int>> evidenceTotals = new Dictionary<string, Tuple<int, int>>;
+
+        // stores number of entries per outcome
+        private Dictionary<string, int> outcomeTotals = new Dictionary<string, int>();
+
+        // stores number of total entries
+        private int totalOutcomeEntries = 0;
+
+        // adds outcome to outcomeSet and updates evidence trues and totals in evidenceTotals
+        public void addOutcome(string outcome, Dictionary<string, Tuple<int, int>> evidence)
+        {
+            outcomeSet.Add(outcome, evidence);
+
+            if (!outcomeTotals.ContainsKey(outcome))
+            {
+                outcomeTotals[outcome] = 0;
+            }
+
+            int outcomeTotal = 0;
+
+            foreach (KeyValuePair<string, Tuple<int, int>> entry in evidence)
+            {
+                if (!evidenceTotals.ContainsKey(entry.Key))
+                {
+                    evidenceTotals[entry.Key] = Tuple.Create(entry.Value.Item1, entry.Value.Item2);
+                } else
+                {
+                    evidenceTotals[entry.Key] = Tuple.Create(evidenceTotals[entry.Key].Item1 + entry.Value.Item1, 
+                        evidenceTotals[entry.Key].Item2 + entry.Value.Item2);
+                }
+
+                outcomeTotal += entry.Value.Item2;
+            }
+
+            outcomeTotals[outcome] = outcomeTotal;
+            totalOutcomeEntries += outcomeTotal;
+        }
+
+        // removes outcome to outcomeSet and updates evidence trues and totals in evidenceTotals
+        public void removeOutcome(string outcome)
+        {
+            foreach (KeyValuePair<string, Tuple<int, int>> entry in outcomeSet[outcome])
+            {
+                evidenceTotals[entry.Key] = Tuple.Create(evidenceTotals[entry.Key].Item1 - entry.Value.Item1,
+                    evidenceTotals[entry.Key].Item2 - entry.Value.Item2);
+            }
+
+            totalOutcomeEntries -= outcomeTotals[outcome];
+            outcomeTotals.Remove(outcome);
+            outcomeSet.Remove(outcome);
+        }
+
+        public Dictionary<string, Dictionary<string, Tuple<int, int>>> Outcomes
+        {
+            get
+            {
+                return outcomeSet;
+            }
+        }
+
+        public Dictionary<string, Tuple<int, int>> TotalEvidences
+        {
+            get
+            {
+                return evidenceTotals;
+            }
+        }
+
+        public Dictionary<string, int> OutcomeTotals
+        {
+            get
+            {
+                return outcomeTotals;
+            }
+        }
+
+        public int TotalOutcomeEntries
+        {
+            get
+            {
+                return totalOutcomeEntries;
+            }
+        }
+    }
+}
